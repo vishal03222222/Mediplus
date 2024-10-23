fe4387405347e9ef3cfb2f5c44a9cb481af81b42
vishal <mgvishalctr@gmail.com>
2024-10-23 23:26:32 +0530

    do

--- mediplus-template/src/App.js
+++ mediplus-template/src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Blogsingle from './components/Blogsingle';
+import Contact from './components/Contact';
+import Profiledetails from './components/Profiledetails';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Profiledetails/>
+     <Blogsingle/>
+     <Contact/>
+
     </div>
   );
 }
---
+++ mediplus-template/src/components/Profiledetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profiledetails = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Profiledetails
