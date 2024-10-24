63d958a054447ee170796fd2336f5f7cf16578a2
vishal <mgvishalctr@gmail.com>
2024-10-24 14:04:29 +0530

    changesupdated

--- mediplus-template/src/App.js
+++ mediplus-template/src/App.js
@@ -1,15 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Blogsingle from './components/Blogsingle';
-import Contact from './components/Contact';
-import Profiledetails from './components/Profiledetails';
+
 
 function App() {
   return (
     <div className="App">
-      <Profiledetails/>
-     <Blogsingle/>
-     <Contact/>
+     
 
     </div>
   );
